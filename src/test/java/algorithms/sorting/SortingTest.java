da4ad9b295c1ad61e5f5be6faec184e73baef900
Jonathan <goprokonto4@gmail.com>
2022-01-18 19:04:54 +0100

    Now it builds again

--- src/test/java/algorithms/sorting/SortingTest.java
+++ src/test/java/algorithms/sorting/SortingTest.java
@@ -4,15 +4,9 @@ import java.util.Arrays;
 import java.util.Random;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-<<<<<<< HEAD
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-=======
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import utils.ArrayUtils;
->>>>>>> dev
 
 /**
  * This class tests all sorting algorithms.
@@ -30,62 +24,15 @@ public class SortingTest {
      */
     @BeforeEach
     public void setup() {
-<<<<<<< HEAD
-        mixedUnsorted = createMixedArray(-1000, 1000, 100);
-        mixedSorted = Arrays.copyOf(mixedUnsorted, mixedUnsorted.length);
-        Arrays.sort(mixedSorted);
-
-        positiveUnsorted = createPositiveArray(1, 1000, 100);
-=======
         mixedUnsorted = ArrayUtils.createMixedArray(-1000, 1000, 100);
         mixedSorted = Arrays.copyOf(mixedUnsorted, mixedUnsorted.length);
         Arrays.sort(mixedSorted);
 
         positiveUnsorted = ArrayUtils.createPositiveArray(1, 1000, 100);
->>>>>>> dev
         positiveSorted = Arrays.copyOf(positiveUnsorted, positiveUnsorted.length);
         Arrays.sort(positiveSorted);
     }
 
-<<<<<<< HEAD
-    /**
-     * A helper function to generate an array of random positive and negative integers.
-     *
-     * @param lowerBound the lower bound of integers (negative)
-     * @param upperBound the upper bound of integers (positive)
-     * @param length     the length of the wanted array
-     * @return the generated array
-     */
-    public int[] createMixedArray(final int lowerBound, final int upperBound, final int length) {
-        final int[] array = new int[length];
-        for (int i = 0; i < length; i++) {
-            if (i % 2 == 0) {
-                array[i] = random.nextInt(upperBound);
-            } else {
-                array[i] = -random.nextInt(Math.abs(lowerBound));
-            }
-        }
-        return array;
-    }
-
-    /**
-     * A helper function to generate an array of random positive integers.
-     *
-     * @param lowerBound the lower bound of integers (positive)
-     * @param upperBound the upper bound of integers (positive)
-     * @param length     the length of the wanted array
-     * @return the generated array
-     */
-    public int[] createPositiveArray(final int lowerBound, final int upperBound, final int length) {
-        assert lowerBound < upperBound;
-        final int[] array = new int[length];
-        for (int i = 0; i < length; i++) {
-            array[i] = lowerBound + random.nextInt(upperBound - lowerBound);
-        }
-        return array;
-    }
-=======
->>>>>>> dev
 
     /* ********************************************************
     THE FOLLOWING METHODS TEST DIFFERENT SORTING ALGORITHMS.
